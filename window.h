e093527bea381b8c745a9fceff2b6a38808bc829
Shantanu Baddar <shantanu.baddar2006@gmail.com>
2026-03-16 23:46:34 +0530

    Add window navigation support to window.h

--- window.h
+++ window.h
@@ -5,6 +5,7 @@
 #include "app_index.h"
 #include "config.h"
 #include "file_nav.h"
+#include "win_nav.h"
 
 typedef struct {
     GtkWidget *entry;
@@ -15,8 +16,10 @@ typedef struct {
     GList *current_matches;
     gboolean is_visible;
     GList *current_file_results;    /* Current file search results */
+    GList *current_win_results;     /* Current window search results */
     gboolean suppress_entry_change; /* Suppress on_entry_changed re-trigger */
     guint file_auto_timeout;        /* Debounce timeout for auto-fill */
+    guint win_auto_timeout;         /* Debounce timeout for window auto-focus */
 } WindowData;
 
 GtkWidget *create_launcher_window(GtkApplication *app, AppIndex *index, Config *config, WindowData **out_data);
