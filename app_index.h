e72a3f7e6accb8c248db7bbab1a61d76191623ce
Shantanu Baddar <shantanu.baddar2006@gmail.com>
2026-03-29 19:32:02 +0530

    Add async load functionality to AppIndex

--- app_index.h
+++ app_index.h
@@ -14,11 +14,18 @@ typedef struct {
 typedef struct {
     GHashTable *apps;  /* name -> AppEntry */
     GList *app_list;   /* List of all AppEntry* for iteration */
+    gboolean loaded;   /* TRUE once loading is complete */
+    gboolean loading;  /* TRUE while async load in progress */
 } AppIndex;
+
+/* Callback for async load completion */
+typedef void (*AppIndexLoadCallback)(AppIndex *index, gpointer user_data);
 
 AppIndex *app_index_new(void);
 void app_index_load(AppIndex *index);
+void app_index_load_async(AppIndex *index, AppIndexLoadCallback callback, gpointer user_data);
 void app_index_free(AppIndex *index);
 int app_index_get_count(AppIndex *index);
+gboolean app_index_is_ready(AppIndex *index);
 
 #endif /* APP_INDEX_H */
