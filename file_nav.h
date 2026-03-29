4d24371363696c8f033cd301b1a9ca2bdc0bd479
Shantanu Baddar <shantanu.baddar2006@gmail.com>
2026-03-29 19:34:17 +0530

    Add function to open file with specified application

--- file_nav.h
+++ file_nav.h
@@ -20,6 +20,7 @@ GList *file_nav_search(const char *dir_path, const char *query, int max_results)
 
 void file_nav_open_file_manager(const char *path);
 void file_nav_open_default(const char *path);
+void file_nav_open_with(const char *path, const char *app);
 void file_entry_free(FileEntry *entry);
 
 #endif /* FILE_NAV_H */
