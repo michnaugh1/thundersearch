1b6783cad1069ffe7a66e07b622661f1b447896e
Shantanu Baddar <shantanu.baddar2006@gmail.com>
2026-03-29 19:33:13 +0530

    Add file_openers field to Config struct

--- config.h
+++ config.h
@@ -4,11 +4,12 @@
 #include <glib.h>
 
 typedef struct {
-    GHashTable *nicknames;    /* nickname -> real app name */
-    GHashTable *usage_counts; /* app name -> launch count */
+    GHashTable *nicknames;      /* nickname -> real app name */
+    GHashTable *usage_counts;   /* app name -> launch count */
+    GHashTable *file_openers;   /* ".ext" -> app command */
     char *config_path;
     char *history_path;
-    char *default_dir;        /* default directory for /fd command */
+    char *default_dir;          /* default directory for /fd command */
 } Config;
 
 Config *config_new(void);
@@ -20,5 +21,6 @@ const char *config_resolve_nickname(Config *config, const char *query);
 void config_increment_usage(Config *config, const char *app_name);
 int config_get_usage_count(Config *config, const char *app_name);
 const char *config_get_default_dir(Config *config);
+const char *config_get_opener(Config *config, const char *filename);
 
 #endif /* CONFIG_H */
