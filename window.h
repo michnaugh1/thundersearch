21e0e3b8e23c9399678ab55e5b75cd19d72d5c53
Shantanu Baddar <shantanu.baddar2006@gmail.com>
2026-04-13 14:29:54 +0530

    Add new widgets for window management

--- window.h
+++ window.h
@@ -6,11 +6,17 @@
 #include "config.h"
 #include "file_nav.h"
 #include "win_nav.h"
+#include "animation.h"
 
 typedef struct {
     GtkWidget *entry;
     GtkWidget *listbox;
     GtkWidget *window;
+    GtkWidget *main_container;      /* Main vertical box */
+    GtkWidget *search_pill;         /* Pill-shaped search container */
+    GtkWidget *results_revealer;    /* GtkRevealer for results */
+    GtkWidget *results_container;   /* Box inside revealer */
+    GtkWidget *scrolled_window;     /* Scrolled window for results */
     AppIndex *index;
     Config *config;
     GList *current_matches;
@@ -20,6 +26,9 @@ typedef struct {
     gboolean suppress_entry_change; /* Suppress on_entry_changed re-trigger */
     guint file_auto_timeout;        /* Debounce timeout for auto-fill */
     guint win_auto_timeout;         /* Debounce timeout for window auto-focus */
+    AnimationState *show_anim;      /* Window show animation */
+    guint anim_tick_id;             /* Animation tick source ID */
+    gboolean hiding;                /* TRUE during hide animation */
 } WindowData;
 
 GtkWidget *create_launcher_window(GtkApplication *app, AppIndex *index, Config *config, WindowData **out_data);
